01043c951279a32ebe89bbbca8dc3bbc9efa1ebb
Luana Pereira <luanapereira4516@gmail.com>
2025-05-13 14:28:49 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-
+public class HelloWordFabrica {
 public class Main {
-    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    public static void main(String[] args) 
 
         System.out.printf("Hello, Word!");
 
